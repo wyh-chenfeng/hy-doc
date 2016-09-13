2dd37f5eeb0a515513b952f2c65c3d66303f11fc
xiaolyuh <xiaolyuh@163.com>
2016-09-13 09:12:02 +0800

    表设计修改

--- 数据库设计/增量SQL.sql
+++ 数据库设计/增量SQL.sql
@@ -23,8 +23,16 @@ CREATE TABLE `t_cases_detail` (
   `image` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
   `create_time` datetime NOT NULL,
-  `update` datetime NOT NULL,
+  `update_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `cases_id` (`cases_id`),
   CONSTRAINT `t_cases_detail_ibfk_1` FOREIGN KEY (`cases_id`) REFERENCES `t_cases` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 添加摘要
+ALTER TABLE `t_news`
+MODIFY COLUMN `id`  bigint(20) NOT NULL AUTO_INCREMENT FIRST ,
+MODIFY COLUMN `type`  int(4) NOT NULL COMMENT '1:公司，2行业' AFTER `id`,
+CHANGE COLUMN `content` `summary`  varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `title`,
+ADD COLUMN `content`  text NOT NULL AFTER `summary`;
+
